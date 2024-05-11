9f9000d3220a57ad2eeab76907b7a20625876af5
Marcio Amoedo <amoedo@prodam.am.gov.br>
2024-05-11 13:46:50 -0400

    Aula de manhã

---
+++ venda/test/interface/servico_cep_test.dart
@@ -0,0 +1,40 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+import 'package:venda/interface/servico_cep.dart';
+
+// Como usar o pacote do Mock
+class ServicoCepFake extends Mock implements ServicoCep {}
+
+void main() {
+  // ServicoCep é uma interface
+  late ServicoCep servicoCep;
+  late String endereco;
+  setUp(() {
+    // Preparação para todos os testes
+    servicoCep = ServicoCepFake();
+    endereco = '''
+{
+  "cep": "69077-795",
+  "logradouro": "Rua José de Alencar",
+  "complemento": "",
+  "bairro": "Japiim",
+  "localidade": "Manaus",
+  "uf": "AM",
+  "ibge": "1302603",
+  "gia": "",
+  "ddd": "92",
+  "siafi": "0255"
+}
+''';
+  });
+  group('Deve testar o serviço de CEP', () {
+    test('Deve obter Endereço pelo CEP', () {
+      // Preparação
+      when(() => servicoCep.obterEndereco('69077795')).thenReturn(endereco);
+      // Execução
+      final enderecoResultado = servicoCep.obterEndereco('69077795');
+      // Verificação
+      expect(enderecoResultado, endereco);
+    });
+  });
+}
