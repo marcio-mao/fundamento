018ef65f964758a6184f4ca9b8c8c464d349d2ef
Marcio Amoedo <marcio_mao@yahoo.com.br>
2024-05-25 09:48:02 -0400

    TEste

---
+++ venda/test/api_brasil_test.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+import 'package:http/http.dart';
+import 'package:test/test.dart';
+
+void main() {
+  late Map<String, String> headers;
+  late Response resposta;
+  late String resultado;
+  late Map resultadoData;
+
+  setUp(() {
+    headers = {
+      'Content-type': 'application/json',
+      'Accept': 'application/json',
+    };
+  });
+  group('Deve testar os serviços de CNPJ da APiBrasil', () {
+    test('Deve verificar se Razão Social é do IFAM', () async {
+      //Arrange
+      final ifamCnpj = '10792928000614';
+      final razaoSocial = 'INSTITUTO FEDERAL DO AMAZONAS';
+
+      //Act
+      resposta = (await get(
+          Uri.parse('https://brasilapi.com.br/api/cnpj/v1/$ifamCnpj'),
+          headers: headers));
+      resultado = utf8.decode(resposta.bodyBytes);
+      resultadoData = json.decode(resultado);
+
+      //Assert
+      expect(resultadoData['razao_social'], razaoSocial);
+    });
+  });
+}
